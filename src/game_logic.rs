8f88c60abe4e925c7c1351c64c19065829be8dff
Michal Svačina <msvacina@outlook.com>
2023-06-29 22:25:01 +0200

    Added more game logic

--- src/game_logic.rs
+++ src/game_logic.rs
@@ -1,4 +1,4 @@
-use crate::case::Case;
+use crate::{case::Case, menu};
 
 pub struct GameLogic {
     pub players_case: Case,
@@ -18,5 +18,49 @@ impl GameLogic {
             opened_cases: Vec::new(),
             round: 0,
         }
+    }
+
+    pub fn choose_case(&mut self) {
+        let case: u8;
+        loop {
+            let input = menu::get_user_input("Please choose a case number between 1 and 26:");
+            case = input.trim().parse().expect("Failed to parse input");
+    
+            if (case < 1) || (case > 26) {
+                println!("Invalid case number!");
+                menu::press_enter();
+            }
+            break;
+        }
+    
+        println!("You chose case number {}!", case);
+        menu::press_enter();
+
+        self.players_case = Case {
+            number: case,
+            value: 0,
+        };
+    }
+    
+    pub fn start_game() {
+        // Generate values for all cases
+        let mut case_value_vector = Self::generate_case_values();
+    }
+
+    fn generate_case_values() -> Vec<usize> {
+        let mut values: Vec<usize> = Vec::new();
+        let mut rng = rand::thread_rng();
+        let mut values_left = vec![
+            1, 10, 100, 500, 1000, 5000, 10000, 25000, 50000, 75000, 100000, 200000, 300000,
+            400000, 500000, 750000, 1000000,
+        ];
+    
+        for _ in 0..26 {
+            let index = rng.gen_range(0, values_left.len());
+            values.push(values_left[index]);
+            values_left.remove(index);
+        }
+    
+        values
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -15,28 +15,7 @@ fn main() {
     menu::press_enter();
     menu::clear_screen();
 
-    let game = game_logic::GameLogic::new();
-
-    let case = choose_case();
-}
-
-fn choose_case() -> Case {
-    let case: u8;
-    loop {
-        let input = menu::get_user_input("Please choose a case number between 1 and 26:");
-        case = input.trim().parse().expect("Failed to parse input");
-
-        if (case < 1) || (case > 26) {
-            println!("Invalid case number!");
-            menu::press_enter();
-        }
-        break;
-    }
-
-    println!("You chose case number {}!", case);
-    menu::press_enter();
-    return Case {
-        number: case,
-        value: 0,
-    };
+    let mut game = game_logic::GameLogic::new();
+    game.choose_case();
+    
 }
